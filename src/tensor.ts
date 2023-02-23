d46ae58442d998ee4f7af6a730219885fdfc4ccf
Andrew Huth <git@huth.me>
2023-02-22 20:48:39 -0500

    Add Scalar type

--- src/tensor.ts
+++ src/tensor.ts
@@ -1,3 +1,4 @@
+type Scalar = number;
 type Tensor<T> = T[];
 
 export function at<T>(t: Tensor<T>, index: number): T {
@@ -8,14 +9,13 @@ export function len<T>(t: Tensor<T>): number {
   return t.length;
 }
 
-export function rank<T>(t: Tensor<T>, acc = 1): number {
-  if (isScalar(at(t, 0))) {
+export function rank(t: Scalar | Tensor<any>, acc = 0): number {
+  if (isScalar(t)) {
     return acc;
   }
-  return rank(at(t, 0) as Tensor<T>, acc + 1);
+  return rank(at(t, 0), acc + 1);
 }
 
-function isScalar(el: any): boolean {
-  // return typeof el === 'number' || typeof el === 'string' || el == null;
-  return !Array.isArray(el);
+function isScalar(el: any): el is Scalar {
+  return typeof el === 'number';
 }
