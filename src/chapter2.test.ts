a6d72505814aa2be17d5cac2c2f2c84f16531616
Andrew Huth <git@huth.me>
2023-02-22 20:31:40 -0500

    Chapter 2 #23

--- src/chapter2.test.ts
+++ src/chapter2.test.ts
@@ -1,4 +1,4 @@
-import {len} from './tensor';
+import {len, rank} from './tensor';
 
 test('len [17 12 91 67]', () => {
   expect(len([17, 12, 91, 67])).toEqual(4);
@@ -6,4 +6,8 @@ test('len [17 12 91 67]', () => {
 
 test('len [[3 2 8] [7 1 9]]', () => {
   expect(len([[3, 2, 8], [7, 1, 9]])).toEqual(2);
+});
+
+test('rank [[[8 9] [4 7]]]', () => {
+  expect(rank([[[8, 9], [4, 7]]])).toEqual(3);
 });
--- src/tensor.ts
+++ src/tensor.ts
@@ -3,3 +3,15 @@ type Tensor<T> = T[];
 export function len<T>(t: Tensor<T>): number {
   return t.length;
 }
+
+export function rank<T>(t: Tensor<T>, acc = 1): number {
+  if (isScalar(t[0])) {
+    return acc;
+  }
+  return rank(t[0] as Tensor<T>, acc + 1);
+}
+
+function isScalar(el: any): boolean {
+  // return typeof el === 'number' || typeof el === 'string' || el == null;
+  return !Array.isArray(el);
+}
