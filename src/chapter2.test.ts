3b5ad92a140db38c8f37d36898bb5ca52a0b5cbe
Andrew Huth <git@huth.me>
2023-02-23 10:55:47 -0500

    Add parens to the pseudo-scheme test names

--- src/chapter2.test.ts
+++ src/chapter2.test.ts
@@ -1,29 +1,29 @@
 import {len, rank, shape} from './tensor';
 
-test('len [17 12 91 67]', () => {
+test('(len [17 12 91 67])', () => {
   expect(len([17, 12, 91, 67])).toEqual(4);
 });
 
-test('len [[3 2 8] [7 1 9]]', () => {
+test('(len [[3 2 8] [7 1 9]])', () => {
   expect(len([[3, 2, 8], [7, 1, 9]])).toEqual(2);
 });
 
-test('rank 8', () => {
+test('(rank 8)', () => {
   expect(rank(8)).toEqual(0);
 });
 
-test('rank [[[8 9] [4 7]]]', () => {
+test('(rank [[[8 9] [4 7]]])', () => {
   expect(rank([[[8, 9], [4, 7]]])).toEqual(3);
 });
 
-test('shape [[5.2 6.3 8.0] [6.9 7.1 0.5]]', () => {
+test('(shape [[5.2 6.3 8.0] [6.9 7.1 0.5]])', () => {
   expect(shape([[5.2, 6.3, 8.0], [6.9, 7.1, 0.5]])).toEqual([2, 3]);
 });
 
-test('shape [[[5] [6] [8]] [[7] [9] [5]]]', () => {
+test('(shape [[[5] [6] [8]] [[7] [9] [5]]])', () => {
   expect(shape([[[5], [6], [8]], [[7], [9], [5]]])).toEqual([2, 3, 1]);
 });
 
-test('shape 9', () => {
+test('(shape 9)', () => {
   expect(shape(9)).toEqual([]);
 });
